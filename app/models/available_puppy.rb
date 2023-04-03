c71ec6e41cb830013be2714574d213ed1193daaa
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-04-03 12:31:42 +0300

    setup models

--- app/models/available_puppy.rb
+++ app/models/available_puppy.rb
@@ -1,2 +1,14 @@
 class AvailablePuppy < ApplicationRecord
+    # add associations
+    belongs_to :father, class_name: "AvailablePuppy", optional: true
+    belongs_to :mother, class_name: "AvailablePuppy", optional: true
+   
+    # add apropriate validations
+    validates :name, :date_of_birth, :picture, :weight, :description, :gender, :father, :mother, presence: true
+    validates :wolf_percentage, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+    validates :availability, inclusion: { in: [true, false] }
+
+
+
+
 end
--- app/models/female_wolf.rb
+++ app/models/female_wolf.rb
@@ -1,2 +1,9 @@
 class FemaleWolf < ApplicationRecord
+    # add associations has many available puppies
+    has_many :available_puppies
+
+    # add apropriate validations
+    validates :name, :date_of_birth, :picture, :weight, :description, presence: true
+    validates :wolf_percentage, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+    
 end
